020f37e3fafff39a994bf051cac431024266ce1d
Thanura <thanudilan@ymail.com>
2025-05-20 09:02:41 +0530

    Committed applitools configration

---
+++ applitools.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  appName: 'Ghost CMS',
+  batchName: 'Ghost CMS Visual Batch',
+  browser: [
+    { width: 1200, height: 800, name: 'chrome' },
+    { width: 1200, height: 800, name: 'firefox' },
+    { width: 1200, height: 800, name: 'edgechromium' }
+  ],
+  viewportSize: { width: 1200, height: 800 },
+  showLogs: true
+};
--- cypress.config.js
+++ cypress.config.js
@@ -15,3 +15,6 @@ module.exports = defineConfig({
     },
   },
 });
+
+
+require('@applitools/eyes-cypress')(module);
